c6482a09f568efff3837c776a74fe5592e4b86cc
ravelojaona tahina natanaela <ravelojaonanatanaela8@gmail.com>
2020-06-23 16:55:31 +0300

    integration observation sur interface finance

--- Controller/ControlFinanceValidationCheque.php
+++ Controller/ControlFinanceValidationCheque.php
@@ -1,5 +1,5 @@
 <?php
-
+//UPLOAD
 function loadclass($class){
        
     require_once "../Model/".$class.'.class.php';
@@ -15,38 +15,38 @@ $etudiant=new Etudiant($data);
 if(isset($_POST['motif'])){
     switch ($_POST['motif']) {
         case 'inscription':
-            $data=array($_POST['matricule'],$_POST['idcheque']);
-            $comptable->ValiderInscriptionViaCheque($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idcheque'],$_POST['observation']);
+            $comptable->ValiderInscriptionViaCheque($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement inscription par cheque","Votre droit pour l'inscription a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='inscription'&mode='cheque'");
             break;
         case 'ecolage' :
-            $data=array($_POST['quantite'],$_POST['matricule'],$_POST['idcheque']);
-            $comptable->ValiderEcolageViaCheque($data[0],$data[1],$data[2]);
+            $data=array($_POST['quantite'],$_POST['matricule'],$_POST['idcheque'],$_POST['observation']);
+            $comptable->ValiderEcolageViaCheque($data[0],$data[1],$data[2],$data[3]);
             mail($etudiant->getMail(),"E-media paiement ecolage par cheque","Votre ecolage a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='ecolage'&mode='cheque'");
             break;
         case 'droit examen semestriel' : 
-            $data=array($_POST['matricule'],$_POST['idcheque']);
-            $comptable->ValiderDroitExamenViaCheque($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idcheque'],$_POST['observation']);
+            $comptable->ValiderDroitExamenViaCheque($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement droit examen semestriel par cheque","Votre droit pour l'examen semestriel a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='des'&mode='cheque'");
             break;
         case 'Droit de soutenance' :
-            $data=array($_POST['matricule'],$_POST['idcheque']);
-            $comptable->ValiderSoutenanceViaCheque($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idcheque'],$_POST['observation']);
+            $comptable->ValiderSoutenanceViaCheque($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement droit de soutenance par cheque","Votre droit de soutenance a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='ds'&mode='cheque'");
             break;
         case 'repechage' :
-            $data=array($_POST['idetudiants'],$_POST['idcheque']);
-            $comptable->ValiderRepechageViaCheque($data[0],$data[1]);
+            $data=array($_POST['idetudiants'],$_POST['idcheque'],$_POST['observation']);
+            $comptable->ValiderRepechageViaCheque($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement repechage par cheque","Votre droit de repechage a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='repechage'&mode='cheque'");
             break;
         case 'certificat' :
-            $data=array($_POST['matricule'],$_POST['idcheque']);
-            $comptable->ValiderCertificat($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idcheque'],$_POST['observation']);
+            $comptable->ValiderCertificat($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement certificat par cheque","Votre droit de certificat a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='certificat'&mode='cheque'");
             break;
--- Controller/ControlFinanceValidationWestern.php
+++ Controller/ControlFinanceValidationWestern.php
@@ -1,4 +1,5 @@
 <?php
+//UPLOAD
 function loadclass($class){
        
     require_once "../Model/".$class.'.class.php';
@@ -15,38 +16,38 @@ $etudiant=new Etudiant($data);
 if(isset($_POST['motif'])){
     switch ($_POST['motif']) {
         case 'inscription':
-            $data=array($_POST['matricule'],$_POST['idwestern']);
-            $comptable->ValiderInscriptionViaWestern($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idwestern'],$_POST['observation']);
+            $comptable->ValiderInscriptionViaWestern($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement inscription par Western Union","Votre droit pour l'inscription a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='inscription'&mode='western'");
             break;
         case 'ecolage' :
-            $data=array($_POST['quantite'],$_POST['matricule'],$_POST['idwestern']);
-            $comptable->ValiderEcolageViaWestern($data[0],$data[1],$data[2]);
+            $data=array($_POST['quantite'],$_POST['matricule'],$_POST['idwestern'],$_POST['observation']);
+            $comptable->ValiderEcolageViaWestern($data[0],$data[1],$data[2],$data[3]);
             mail($etudiant->getMail(),"E-media paiement ecolage par Western union","Votre ecolage a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='ecolage'&mode='western'");
             break;
         case 'droit examen semestriel' : 
-            $data=array($_POST['matricule'],$_POST['idwestern']);
-            $comptable->ValiderDroitExamenViaWestern($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idwestern'],$_POST['observation']);
+            $comptable->ValiderDroitExamenViaWestern($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement droit examen semestriel par western","Votre droit pour l'examen semestriel a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='des'&mode='western'");
             break;
         case 'Droit de soutenance' :
-            $data=array($_POST['matricule'],$_POST['idwestern']);
-            $comptable->ValiderSoutenanceViaWestern($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idwestern'],$_POST['observation']);
+            $comptable->ValiderSoutenanceViaWestern($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement droit de soutenance par western","Votre droit de soutenance a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='ds'&mode='western'");
             break;
         case 'repechage' :
-            $data=array($_POST['idetudiants'],$_POST['idwestern']);
-            $comptable->ValiderRepechageViaWestern($data[0],$data[1]);
+            $data=array($_POST['idetudiants'],$_POST['idwestern'],$_POST['observation']);
+            $comptable->ValiderRepechageViaWestern($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement repechage par western","Votre droit de repechage a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='repechage'&mode='western'");
             break;
         case 'certificat' :
-            $data=array($_POST['matricule'],$_POST['idwestern']);
-            $comptable->ValiderCertificat($data[0],$data[1]);
+            $data=array($_POST['matricule'],$_POST['idwestern'],$_POST['observation']);
+            $comptable->ValiderCertificat($data[0],$data[1],$data[2]);
             mail($etudiant->getMail(),"E-media paiement certificat par western","Votre droit de certificat a ete valide");
             header("location:../Vue/dashboard.php?status='valider'&motif='certificat'&mode='western'");
             break;
